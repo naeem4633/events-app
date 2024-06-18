1fe3982f89a91ea6d667f98681901c54ff072e15
Ali Arshad <130868985+Aliarshad0014@users.noreply.github.com>
2024-06-18 21:05:48 +0500

    fixed login

--- frontend/src/components/Header/MainNav1.tsx
+++ frontend/src/components/Header/MainNav1.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Logo from "shared/Logo/Logo";
 import Navigation from "shared/Navigation/Navigation";
 import SearchDropdown from "./SearchDropdown";
@@ -6,12 +6,20 @@ import ButtonPrimary from "shared/Button/ButtonPrimary";
 import MenuBar from "shared/MenuBar/MenuBar";
 import SwitchDarkMode from "shared/SwitchDarkMode/SwitchDarkMode";
 import HeroSearchForm2MobileFactory from "components/HeroSearchForm2Mobile/HeroSearchForm2MobileFactory";
+import logoImg from "images/HIW1.png";
+
 
 export interface MainNav1Props {
   className?: string;
 }
 
 const MainNav1: FC<MainNav1Props> = ({ className = "" }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div className={`nc-MainNav1 relative z-10 ${className}`}>
       <div className="px-4 lg:container py-4 lg:py-5 relative flex justify-between items-center">
@@ -29,7 +37,23 @@ const MainNav1: FC<MainNav1Props> = ({ className = "" }) => {
             <SwitchDarkMode />
             <SearchDropdown />
             <div className="px-1" />
-            <ButtonPrimary href="/login">Sign up</ButtonPrimary>
+            {isLoggedIn ? (
+              <div className="relative">
+                <a href="/Account">
+                  <div className="h-10 w-10 rounded-full overflow-hidden">
+                    <img
+                      className="h-full w-full object-cover"
+                      src={logoImg}
+                      alt="User"
+                    />
+                  </div>
+                </a>
+              </div>
+            ) : (
+              <ButtonPrimary href="/login" onClick={handleLogin}>
+                Login
+              </ButtonPrimary>
+            )}
           </div>
           <div className="flex xl:hidden items-center">
             <SwitchDarkMode />
